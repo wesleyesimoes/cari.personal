0e9ad04a466ae960035732f35b3e7b296b808d0c
Wesley Espala Simões <wesley.simoes@live.com>
2026-01-10 16:35:29 -0300

    analytics

---
+++ js/analytics.js
@@ -0,0 +1,13 @@
+(function () {
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = "https://www.googletagmanager.com/gtag/js?id=G-6BDLBQ7GBQ";
+    document.head.appendChild(script);
+
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    window.gtag = gtag;
+
+    gtag('js', new Date());
+    gtag('config', 'G-6BDLBQ7GBQ');
+})();
